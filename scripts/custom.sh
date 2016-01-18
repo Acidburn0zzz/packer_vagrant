78a070b4126b063784f0e05ae26cff86aeced805
varac <varacanero@zeromail.org>
2016-01-18 09:45:02 +0100

    install openssl to set root pw

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -3,8 +3,9 @@
 # Allow root login
 sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
+# install additional packages
+# openssl is needed to set root pw later
+apt-get -y install puppet lsb-release facter rsync curl openssl
+
 # Set root pw
 usermod -p "$(echo vagrant | openssl passwd -1 -stdin)" root
-
-# install additional packages
-apt-get -y install puppet lsb-release facter rsync curl
