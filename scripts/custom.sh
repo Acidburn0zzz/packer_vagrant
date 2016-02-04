602f9a306656f09eab2372bdee473c0e54e1294e
kwadronaut <kwadronaut@leap.se>
2016-02-05 00:37:08 +0100

    remove openssl as requirement, use chpasswd for root

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -4,8 +4,7 @@
 sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 # install additional packages
-# openssl is needed to set root pw later
-apt-get -y install puppet lsb-release facter rsync curl openssl
+apt-get -y install puppet lsb-release facter rsync curl
 
 # Set root pw
-usermod -p "$(echo vagrant | openssl passwd -1 -stdin)" root
+echo 'root:vagrant' | chpasswd 
