439ed6fd6b47d4e7e763405784448d3b784cc2a3
varac <varacanero@zeromail.org>
2016-02-09 10:11:17 +0100

    added bash-completion

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -5,7 +5,7 @@ sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_c
 
 # install additional packages
 # openssl is needed to set root pw later
-apt-get -y install puppet lsb-release facter rsync curl openssl
+apt-get -y install puppet lsb-release facter rsync curl openssl bash-completion
 
 # Set root pw
 usermod -p "$(echo vagrant | openssl passwd -1 -stdin)" root
